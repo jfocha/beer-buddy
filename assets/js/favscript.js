b99cd5c260c97368de496a8d85bd8f27d02dd9f1
Tori Dang <mthud2904@gmail.com>
2021-06-16 13:23:34 -0700

    changed

--- assets/js/favscript.js
+++ assets/js/favscript.js
@@ -2,25 +2,30 @@
 
 var update = document.querySelector("#text");
 
-fetch("https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb")
-	.then(response => {
-		return response.json();
-	}).then(data => {
-		console.log(data);
-		makeCardEl(data, true);
+// fetch("https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb")
+// 	.then(response => {
+// 		return response.json();
+// 	}).then(data => {
+// 		console.log(data);
+// 		makeCardEl(data, true);
 
-	})
-	.catch(err => {
-		console.error(err);
-	});
-
+// 	})
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
+var film ="annabelle";
+    $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function(json) {
+        			 if (json != "Nothing found."){                 
+        console.log(json);
+                     }
+                    });
 // var cardEl = document.createElement("div");
 // update.append(cardEl)
 var makeCardEl = function () {
 	// pageEl.innerHTML = "<h2 class='title'>New Releases</h2>"
 	// for (let i = 0; i < 6; i++) {
 		var cardEl = document.createElement("div");
-		cardEl.classList.add("col", "s12", "m2");
+		cardEl.classList.add("col", "s6", "s3");
 
 		var cardContainerEl = document.createElement("div")
 		cardContainerEl.setAttribute("class", "card horizontal");
@@ -28,7 +33,7 @@ var makeCardEl = function () {
 
 		var cardImageEl = document.createElement("div");
 		cardImageEl.setAttribute("class", "card-image");
-		cardImageEl.innerHTML = "<img src='http://image.tmdb.org/t/p/w300"; // + tm.results[i].poster_path + "'></img>";
+		cardImageEl.innerHTML = "<img src='http://image.tmdb.org/t/p/w300/yLsuU2P2SpDYFwtZQ7dtfVAf6TE.jpg'></img>"; //"; // + tm.results[i].poster_path + "'></img>";
 		cardContainerEl.appendChild(cardImageEl);
 		// It's possible to add a title on the image. Use: <span class="card-title">Card Title</span> in the innerHTML after the img.
 
