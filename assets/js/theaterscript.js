7d792f866fadac3df5fc75963b63933f84d42fa7
jfocha <jfocha@gmail.com>
2021-06-18 17:08:21 -0700

    added map tiles

--- assets/js/theaterscript.js
+++ assets/js/theaterscript.js
@@ -1,8 +1,15 @@
 var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
+    center: [37.7185, -122.4363],
+    zoom: 10
 });
 
-L.tileLayer('https://maps.tilehosting.com/styles/streets/{z}/{x}/{y}.png?key=HfiQgsMsSnorjEs2Sxek', {
-    attribution: '<a href="https:www.maptiler.com/license/maps/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target"_blank">© OpenStreetMap contributors</a>', 
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9ib3QxYSIsImEiOiJja3Eyem54aGUwdG5sMnVvNHhxYzE0cmJ0In0.QKQ9Dw_CmnmhmVNE-IvEUw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
 }).addTo(map);
+
+// default public token: pk.eyJ1Ijoicm9ib3QxYSIsImEiOiJja3Eyem54aGUwdG5sMnVvNHhxYzE0cmJ0In0.QKQ9Dw_CmnmhmVNE-IvEUw
